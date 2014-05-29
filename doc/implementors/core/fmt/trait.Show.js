b0e76378cf28756e8e2d5b057c6b538710e0d2b7
Steven Fackler <sfackler@gmail.com>
2014-05-28 21:55:15 -0700

    Update docs

--- doc/implementors/core/fmt/trait.Show.js
+++ doc/implementors/core/fmt/trait.Show.js
@@ -1,5 +1,5 @@
 (function() {var implementors = {};
-implementors['postgres'] = {PostgresType:"postgres/types/type.PostgresType.html",Range:"postgres/types/range/struct.Range.html",RangeBound:"postgres/types/range/struct.RangeBound.html",DimensionInfo:"postgres/types/array/struct.DimensionInfo.html",PostgresError:"postgres/error/type.PostgresError.html",PostgresDbError:"postgres/error/struct.PostgresDbError.html",PostgresConnectError:"postgres/error/type.PostgresConnectError.html",};
+implementors['postgres'] = {PostgresNotification:"postgres/struct.PostgresNotification.html",PostgresType:"postgres/types/type.PostgresType.html",Range:"postgres/types/range/struct.Range.html",RangeBound:"postgres/types/range/struct.RangeBound.html",DimensionInfo:"postgres/types/array/struct.DimensionInfo.html",PostgresError:"postgres/error/type.PostgresError.html",PostgresDbError:"postgres/error/struct.PostgresDbError.html",PostgresConnectError:"postgres/error/type.PostgresConnectError.html",};
 
                 if (window.register_implementors) {
                     window.register_implementors(implementors);
