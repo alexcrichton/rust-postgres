94d6db9a3d9b78056806af3def9567a146ad9ffe
Steven Fackler <sfackler@gmail.com>
2014-06-16 20:37:22 -0700

    Update docs

---
+++ doc/implementors/core/collections/trait.Collection.js
@@ -0,0 +1,10 @@
+(function() {var implementors = {};
+implementors['postgres'] = ["impl&lt;'stmt&gt; <a class='trait' href='http://doc.rust-lang.org/core/collections/trait.Collection.html' title='core::collections::Collection'>Collection</a> for <a class='struct' href='postgres/struct.PostgresRow.html' title='postgres::PostgresRow'>PostgresRow</a>&lt;'stmt&gt;",];
+
+            if (window.register_implementors) {
+                window.register_implementors(implementors);
+            } else {
+                window.pending_implementors = implementors;
+            }
+        
+})()
