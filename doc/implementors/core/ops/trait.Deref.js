6da0f462859b539a1fada2995dd7d3521ca354a3
Steven Fackler <sfackler@gmail.com>
2014-06-24 23:30:30 -0700

    Update docs

---
+++ doc/implementors/core/ops/trait.Deref.js
@@ -0,0 +1,10 @@
+(function() {var implementors = {};
+implementors['postgres'] = ["impl <a class='trait' href='http://doc.rust-lang.org/core/ops/trait.Deref.html' title='core::ops::Deref'>Deref</a>&lt;<a class='struct' href='postgres/struct.PostgresConnection.html' title='postgres::PostgresConnection'>PostgresConnection</a>&gt; for <a class='struct' href='postgres/pool/struct.PooledPostgresConnection.html' title='postgres::pool::PooledPostgresConnection'>PooledPostgresConnection</a>",];
+
+            if (window.register_implementors) {
+                window.register_implementors(implementors);
+            } else {
+                window.pending_implementors = implementors;
+            }
+        
+})()
